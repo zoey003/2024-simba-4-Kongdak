54a66f39abc6fde1f6cb00f0f63cc14286334314
onlynyang <chica1875@gmail.com>
2024-06-16 16:33:32 +0900

    Feat: 회원가입 및 로그인 기능 개발

---
+++ project/accounts/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.contrib import auth
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('main:mainpage')
+        else:
+            return render(request, 'accounts/login.html')
+
+
+    elif request.method == 'GET':
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('main:mainpage')
+
+
+def signup(request):
+    if request.method == 'POST':
+
+        if request.POST['password'] == request.POST['confirm']:
+            user = User.objects.create_user(
+                username = request.POST['username'],
+                password = request.POST['password']
+            )
+
+            auth.login(request, user)
+            return redirect('/')
+
+    return render(request, 'accounts/signup.html')
