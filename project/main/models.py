9462924dc5cc738750cd28e264d0345a4974a2a6
pyeree <dlagusdn0218@gmail.com>
2024-06-20 16:59:36 +0900

    Feat: DB 구축 및 signup 함수 confirm 구축

--- project/main/models.py
+++ project/main/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    category= models.CharField(max_length=10)
+    subcategory = models.CharField(max_length=10)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30,null=False,blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    category = models.CharField(max_length=50)
-    subcategory = models.CharField(max_length=50)
+    categories = models.ForeignKey(Category,on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    tags = models.ManyToManyField(Tag, related_name='pots',blank=True)
     def __str__(self):
         return self.title
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.TextField(max_length=10)
-    department = models.TextField(null=True, max_length=30)
-    followings = models.ManyToManyField("self", related_name="followers", symmetrical=False)
+    studentID = models.TextField(null=True, max_length=30)
