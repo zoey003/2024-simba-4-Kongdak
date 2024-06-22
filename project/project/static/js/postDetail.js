46278ae5e3f6088ef87987d0b3f8b0785996aa30
Chaem03 <163885086+Chaem03@users.noreply.github.com>
2024-06-23 00:36:40 +0900

    Feat: 글 보는 페이지 및 수정 페이지 개발 완료 (#116)

---
+++ project/project/static/js/postDetail.js
@@ -0,0 +1,44 @@
+const back = document.getElementById("back_edit");
+
+back.addEventListener("click", () => {
+  window.history.back();
+});
+const editBtn = document.getElementById("edit_diary");
+const deleteBtn = document.getElementById("delete_diary");
+const deleteModal = document.getElementById("delete-modal");
+const confirmDelete = document.getElementById("confirm-delete");
+const cancelDelete = document.getElementById("cancel-delete");
+const contentToBlur = document.querySelectorAll("#root > *:not(#delete-modal)");
+const searchBtn = document.querySelector("#edit_textarea");
+
+function toggleBlur(toggle) {
+  contentToBlur.forEach((element) => {
+    if (toggle) {
+      element.classList.add("blurred");
+    } else {
+      element.classList.remove("blurred");
+    }
+  });
+}
+
+deleteBtn.addEventListener("click", () => {
+  deleteModal.classList.remove("hidden-class");
+  toggleBlur(true);
+});
+
+searchBtn.addEventListener("click", function () {
+  deleteModal.classList.add("hidden-class");
+  toggleBlur(false);
+});
+
+confirmDelete.addEventListener("click", function () {
+  window.location.href = deleteUrl;
+});
+
+cancelDelete.addEventListener("click", function () {
+  window.location.href = detailpostUrl;
+});
+
+editBtn.addEventListener("click", function () {
+  window.location.href = editUrl;
+});
