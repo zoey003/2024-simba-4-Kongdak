1f68e230229d44fcf718b0232df6dac3bf56cb4d
onlynyang <163874934+onlynyang@users.noreply.github.com>
2024-06-22 22:42:09 +0900

    Feat: all_posts filter 함수 추가 (#109)
    * Feat: migrations 오류
    
    * Feat: settings.py 수정
    
    * Fix: url 오타 수정
    
    * Fix: 모아보기페이지 filter함수로 수정

--- project/main/views.py
+++ project/main/views.py
@@ -228,9 +228,10 @@ def delete_post(request, category, subcategory, post_id):
 def post_detail(request, category, subcategory, post_id):
     post = get_object_or_404(Post, id=post_id, category=category, subcategory=subcategory)
     return render(request, 'main/post_detail.html', {'post': post, 'next': request.GET.get('next')})
+
 @login_required
 def all_posts(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(author=request.user)
 
     bookmarked_posts = request.user.bookmark.all()
 
