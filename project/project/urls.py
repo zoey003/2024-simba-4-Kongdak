9ecd1994788e519ecab3c55e74fbe305997a8536
pyeree <dlagusdn0218@gmail.com>
2024-06-25 11:43:47 +0900

    Refactor: 백엔드 리펙토링

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main import views
-from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
 ] 
 
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
